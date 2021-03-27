31e21a8665e78698c47d6f2465cb1da5cd4a54e1
yalbee <sacha-bugaev@hotmail.com>
2021-03-27 13:41:09 +0300

    проект родился

---
+++ data/users.py
@@ -0,0 +1,27 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
+
+class Users(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hometown = sqlalchemy.Column(sqlalchemy.String)
+    birthday = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)

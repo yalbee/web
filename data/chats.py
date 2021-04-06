357d69bbb0eff5de62930b1e1b407d5e2373ad00
yalbee <sacha-bugaev@hotmail.com>
2021-04-06 17:27:26 +0300

    aaaaaaaaaaаaaaaaa

--- data/chats.py
+++ data/chats.py
@@ -3,16 +3,22 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy import orm
 
+association_table = sqlalchemy.Table(
+    'users_to_chats',
+    SqlAlchemyBase.metadata,
+    sqlalchemy.Column('user', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('users.id')),
+    sqlalchemy.Column('chat', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('chats.id')))
+
 
 class Chats(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'chats'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
     name = sqlalchemy.Column(sqlalchemy.String)
-    chat = sqlalchemy.Column(sqlalchemy.Integer)
     last_message = sqlalchemy.Column(sqlalchemy.String)
     last_message_dt = sqlalchemy.Column(sqlalchemy.DateTime)
-    user = orm.relation('Users')
+    users = orm.relation('Users', secondary="users_to_chats", backref="chat")
     messages = orm.relation("Messages", back_populates='chat')
